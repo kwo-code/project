3b79dd42b7e7b153a3a5b38ad329511bfe931abc
Kwokki <95371638+kwo-code@users.noreply.github.com>
2023-05-18 00:24:00 +0300

    Add files via upload

--- test/Новый текстовый документ.py
+++ test/Новый текстовый документ.py
@@ -1,85 +1,80 @@
 import pygame
-from pygame.locals import *
-import random
 
 # Инициализация Pygame
 pygame.init()
 
 # Размеры окна
 width = 800
-height = 400
+height = 600
 
 # Цвета
-WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
-
-# Скорость движения ракеток
-PADDLE_SPEED = 5
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
 
 # Создание окна
 screen = pygame.display.set_mode((width, height))
-pygame.display.set_caption("Пинг-понг")
+pygame.display.set_caption("Пример меню")
 
-# Создание ракеток
-paddle_width = 10
-paddle_height = 60
-paddle1_x = 20
-paddle1_y = height // 2 - paddle_height // 2
-paddle2_x = width - paddle_width - 20
-paddle2_y = height // 2 - paddle_height // 2
+# Шрифты
+font_title = pygame.font.Font(None, 60)
+font_options = pygame.font.Font(None, 40)
 
-# Создание мяча
-ball_radius = 10
-ball_x = width // 2
-ball_y = height // 2
-ball_speed_x = random.choice([-2, 2])
-ball_speed_y = random.choice([-2, 2])
+# Флаг для отображения меню
+show_menu = True
 
-# Главный цикл игры
+# Основной цикл программы
 running = True
-clock = pygame.time.Clock()
 while running:
-    clock.tick(60)
-
-    # Обработка событий
     for event in pygame.event.get():
-        if event.type == QUIT:
+        if event.type == pygame.QUIT:
             running = False
 
-    # Обработка нажатий клавиш
-    keys = pygame.key.get_pressed()
-    if keys[K_w] and paddle1_y > 0:
-        paddle1_y -= PADDLE_SPEED
-    if keys[K_s] and paddle1_y < height - paddle_height:
-        paddle1_y += PADDLE_SPEED
-    if keys[K_UP] and paddle2_y > 0:
-        paddle2_y -= PADDLE_SPEED
-    if keys[K_DOWN] and paddle2_y < height - paddle_height:
-        paddle2_y += PADDLE_SPEED
+    if show_menu:
+        screen.fill(BLACK)
 
-    # Обновление координат мяча
-    ball_x += ball_speed_x
-    ball_y += ball_speed_y
+        # Отрисовка заголовка меню
+        title_text = font_title.render("Меню", True, WHITE)
+        title_rect = title_text.get_rect(center=(width / 2, height / 4))
+        screen.blit(title_text, title_rect)
 
-    # Отскок мяча от ракеток
-    if ball_x <= paddle1_x + paddle_width and paddle1_y <= ball_y <= paddle1_y + paddle_height:
-        ball_speed_x = abs(ball_speed_x)
-    if ball_x >= paddle2_x - ball_radius and paddle2_y <= ball_y <= paddle2_y + paddle_height:
-        ball_speed_x = -abs(ball_speed_x)
+        # Отрисовка пунктов меню
+        option1_text = font_options.render("Начать игру", True, WHITE)
+        option1_rect = option1_text.get_rect(center=(width / 2, height / 2))
+        screen.blit(option1_text, option1_rect)
 
-    # Отскок мяча от верхней и нижней стенок
-    if ball_y <= 0 or ball_y >= height - ball_radius:
-        ball_speed_y = -ball_speed_y
+        option2_text = font_options.render("Настройки", True, WHITE)
+        option2_rect = option2_text.get_rect(center=(width / 2, height / 2 + 50))
+        screen.blit(option2_text, option2_rect)
 
-    # Проверка на окончание игры
-    if ball_x < 0 or ball_x > width:
-        running = False
+        option3_text = font_options.render("Выйти", True, WHITE)
+        option3_rect = option3_text.get_rect(center=(width / 2, height / 2 + 100))
+        screen.blit(option3_text, option3_rect)
 
-    # Отрисовка объектов на экране
-    screen.fill(BLACK)
-    pygame.draw.rect(screen, WHITE, (paddle1_x, paddle1_y, paddle_width, paddle_height))
-    pygame.draw.rect(screen, WHITE, (paddle2_x, paddle2_y, paddle_width, paddle_height))
-    pygame.draw.circle(screen, WHITE, (ball_x, ball_y), ball_radius)
+        # Обработка кликов мыши
+        mouse_pos = pygame.mouse.get_pos()
+        mouse_click = pygame.mouse.get_pressed()
+
+        if option1_rect.collidepoint(mouse_pos) and mouse_click[0] == 1:
+            show_menu = False
+            # Здесь можно добавить логику для начала игры
+
+        if option2_rect.collidepoint(mouse_pos) and mouse_click[0] == 1:
+            show_menu = False
+            # Здесь можно добавить логику для открытия настроек
+
+        if option3_rect.collidepoint(mouse_pos) and mouse_click[0] == 1:
+            running = False
+            # Здесь можно добавить логику для выхода из программы
+
+    else:
+        # Здесь можно добавить логику для основной игры или другого экрана
+
+        screen.fill(GREEN)  # Пример: зеленый фон для основной игры
 
     # Обновление экрана
     pygame.display.flip()
+
+# Завершение работы Pygame
+pygame.quit()
